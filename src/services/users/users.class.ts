fdcf6219ac9ac88540e601b9783b1d4c73798de6
evilj0ker <rajatsri94@gmail.com>
2021-01-30 00:30:26 +0530

    add user gravatar image

--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,9 +1,35 @@
+import crypto from 'crypto';
+import {Params} from '@feathersjs/feathers';
 import {Service, NedbServiceOptions} from 'feathers-nedb';
 import {Application} from '../../declarations';
 
-export class Users extends Service {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+const gravatarUrl = 'https://s.gravatar.com/avatar';
+const query = 's=60';
+const getGravatar = (email: string) => {
+  const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+  return `${gravatarUrl}/${hash}?${query}`;
+};
+
+interface UserData {
+  _id?: string;
+  email: string;
+  password: string;
+  name?: string;
+  avatar?: string;
+  githubId?: string;
+}
+
+export class Users extends Service<UserData> {
   constructor(options: Partial<NedbServiceOptions>, app: Application) {
     super(options);
+  }
+
+  create(data: UserData, params?: Params) {
+    const {email, password, githubId, name} = data;
+    const avatar = data.avatar || getGravatar(email);
+    const userData = {
+      email, name, password, githubId, avatar,
+    };
+    return super.create(userData, params);
   }
 };
